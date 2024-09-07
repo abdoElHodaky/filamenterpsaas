2ea9fc030f2a0b6f69060d63cae458245528f430
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-07 17:44:04 +0300

    Update database.php
    Signed-off-by: AbdElrhman ElHodaky <abdo.arh38@yahoo.com>

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL',"postgres://qdcndkih:cfMYmAnwqG6yTsyK5e-ZEedfMdAZBJ"),
+            'url' => env('DATABASE_URL',"postgres://qdcndkih:cfMYmAnwqG6yTsyK5e-ZEedfMdAZBJY_@lallah.db.elephantsql.com/qdcndkih"),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
