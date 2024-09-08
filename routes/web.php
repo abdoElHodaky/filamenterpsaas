a9006e2684e928db958197a4a60cb180727b692c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-08 08:16:56 +0300

    Update web.php
    Signed-off-by: AbdElrhman ElHodaky <abdo.arh38@yahoo.com>

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function () {
+    $prevUrl = url()->previous();
+
+    if (! $prevUrl) {
+        abort(404); // or redirect some where
+    }
+
+    $path = parse_url($prevUrl, PHP_URL_PATH);
+
+    $panelId = explode('/', trim($path, '/'))[0];
+
+    if (! in_array($panelId, array_keys(Filament::getPanels()))) {
+        abort(404);
+    }
+
+    return redirect(route("filament.{$panelId}.auth.login"));
+})->name('login');
